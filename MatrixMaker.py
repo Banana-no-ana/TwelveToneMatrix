808c325f390b2659c74c69eb5ce2e9bfc1c98be7
Daniel Piao <dimshadow101@gmail.com>
2014-01-18 16:26:02 -0800

    Generator works now

--- MatrixMaker.py
+++ MatrixMaker.py
@@ -7,6 +7,8 @@
 
 import random
 
+#Generates a row
+#TODO: Need to normalize prime to 0
 def RowGenerator():
 	rowGenerator = []
 	myRow = []
@@ -15,16 +17,28 @@ def RowGenerator():
 	for x in range(0, 12):
 		myItem = randRemove(rowGenerator)
 		myRow.append(myItem)
+
+	myRow = normalizeRow(myRow)
 	print myRow
 	return myRow
 
+def calcNormalized(item, prime):
+	note = (item + 12 - prime) % 12
+	return note
+
+def normalizeRow(row):
+	prime = row[0]
+	newRow = []
+	for item in row:
+		newRow.append(calcNormalized(item, prime))
+	return newRow
 
 def randRemove(myList):
 	index = random.randint(0, len(myList)-1)
 	if len(myList) == 1:
 		return myList.pop(0)
 	return myList.pop(index)
-	
+
 def printRow(myRow):
 	for item in myRow:
 		print item
@@ -36,7 +50,7 @@ def simpleRowYielder():
 def InverseGenerator(myRow):
 	myInverse = []
 	for item in myRow:
-		note = 12 - item
+		note = (12 - item) % 12
 		myInverse.append(note)
 	print "MyInverse is: ", myInverse
 	return myInverse
@@ -44,18 +58,25 @@ def InverseGenerator(myRow):
 def CalcMatrix(Prime, Inverse):
 	myMatrix = Matrix(12, 12)
 	myMatrix.setRow(0, Prime)
-	for x in range(12):
+	for x in range(1, 12):
 		offset = Inverse[x]
-		print offset
-
-
+		myrow = calcRow(Prime, offset)
+		#Add myrow to matrix
+		myMatrix.setRow(x, myrow)
 	print myMatrix
+
+def calcRow(row, offset):
+	#Add offset to prime
+	newRow = []
+	for item in row:
+		 newRow.append((item + offset) % 12)
+	return newRow
 
 class Matrix(object):
 	def __init__(self, cols, rows):
 		self.cols = cols
 		self.rows = rows
-		
+
 		self.Matrix = []
 		for i in range(rows):
 			ea_row = []
@@ -67,14 +88,23 @@ class Matrix(object):
 
 	def setItem(self, col, row, thing):
 		self.Matrix[col-1][row-1] = thing
-	
+
 	def setRow(self, col, myRow):
 		self.Matrix[col] = myRow
+
+	def rowREPR(self, mylist):
+		outstring = '[ '
+		for item in mylist:
+			outstring = outstring + str(item) + ',  '
+		#Remove last comma
+		outstring = outstring[0:-2]
+		outstring = outstring + '] '
+		return outstring
 
 	def __repr__(self):
 		outStr = ""
 		for i in range(self.rows):
-			outStr += 'Row %s = %s \n' % (i+1, self.Matrix[i])
+			outStr += 'Row %s \t = %s \n' % (i+1, self.rowREPR(self.Matrix[i]))
 		return outStr
 
 
